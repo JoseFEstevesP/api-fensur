4fdbfa8dc1463069cfcf1307150059d4bc44c59e
José Esteves <joseesteves199930@gmail.com>
2023-09-30 19:14:43 -0400

    feat: :sparkles: mas info al momento de solicitar la lista de estudiante
    se agrego una función par tener más datos de los estudiante
    
    #	modified:   src/controllers/student-read.controller.js

--- src/controllers/student-read.controller.js
+++ src/controllers/student-read.controller.js
@@ -1,11 +1,25 @@
+import { Course } from '#Schemas/course.schema.js';
 import { Student } from '#Schemas/student.schema.js';
-
+import { User } from '#Schemas/user.schema.js';
+const getStudentData = async ({ uid, uidStudent, uidCourse }) => {
+  const user = await User.findByPk(uidStudent);
+  const course = await Course.findByPk(uidCourse);
+  return {
+    uid,
+    uidStudent,
+    uidCourse,
+    studentName: user.name,
+    studentSurname: user.surname,
+    courseName: course.name,
+  };
+};
 const studentReadController = async (req, res) => {
   const { page = 1, limit = 10 } = req.query;
   const { rows, count } = await Student.findAndCountAll({
     limit,
     page,
   });
+  const studentWithDetails = await Promise.all(rows.map(getStudentData));
   const pages = Math.ceil(count / limit);
   const totalPage = page > pages ? pages : page;
   const nextPage = Number(totalPage) + 1;
@@ -17,7 +31,7 @@ const studentReadController = async (req, res) => {
     previousPage: previousPage > 0 ? previousPage : null,
     limit: Number(limit),
     pages,
-    rows,
+    studentWithDetails,
   });
 };
 
