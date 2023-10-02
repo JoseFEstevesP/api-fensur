55508502293553f03d081e6dab93a35536e69ebf
José Esteves <joseesteves199930@gmail.com>
2023-10-02 15:45:29 -0400

    refactor: :art: console eliminado
    se elimino el console.log del validador de permisos
    
    #	modified:   src/functions/validatePermissions.js

--- src/functions/validatePermissions.js
+++ src/functions/validatePermissions.js
@@ -2,7 +2,6 @@ import { permissions } from '#Constants/permissions.js';
 import { Rol } from '#Schemas/rol.schema.js';
 export const validatePermissions = async ({ uidRol, per }) => {
   const isPermissionsRol = await Rol.findByPk(uidRol);
-  console.log('validatePermissions -> isPermissionsRol:', isPermissionsRol);
   const validate = isPermissionsRol?.permissions
     .split(',')
     .some((item) => item === per || item === permissions.super);
