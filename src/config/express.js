caa27c2ca16e6e45fbd4fdf1170022da1396aed2
José Esteves <joseesteves199930@gmail.com>
2023-10-04 01:28:56 -0400

    feat: :sparkles: cors
    se agrego el protocolo de las cors, falta espesificar para tener más seguridad
    
    #	modified:   package.json #	modified:   pnpm-lock.yaml #	modified:   src/config/express.js

--- src/config/express.js
+++ src/config/express.js
@@ -4,10 +4,12 @@ import rolRoutes from '#Routes/rol.routes.js';
 import studentRoutes from '#Routes/student.routes.js';
 import teacherRoutes from '#Routes/teacher.routes.js';
 import userRoutes from '#Routes/user.routes.js';
+import cors from 'cors';
 import express from 'express';
 const expressApp = express();
 // middleware
 expressApp.use(express.json());
+expressApp.use(cors());
 // routes
 expressApp.use('/user', userRoutes);
 expressApp.use('/rol', rolRoutes);
